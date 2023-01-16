ce512f98e4ce21ab824c03f71c884ce64e7c9f5a
Zul D Amani <114483118+ReinGrad@users.noreply.github.com>
2023-01-16 15:30:35 +0600

    Add files via upload
      "author": "",
      "license": "ISC",
      "devDependencies": {
        "browser-sync": "^2.26.12",
        "gulp": "^3.9.1",
        "gulp-autoprefixer": "^6.1.0",
        "gulp-clean-css": "^4.3.0",
        "gulp-cli": "^2.3.0",
        "gulp-htmlmin": "^5.0.1",
        "gulp-imagemin": "^4.1.0",
        "gulp-rename": "^1.4.0",
        "gulp-sass": "^5.0.0",
        "sass": "^1.35.1"

--- gulpfile.js
+++ gulpfile.js
@@ -1,111 +1,71 @@
-const gulp = require('gulp'); // Подключаем Gulp
-const browserSync = require('browser-sync').create();
-const watch = require('gulp-watch');
-const sass = require('gulp-sass');
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const notify = require('gulp-notify');
-const plumber = require('gulp-plumber');
-const pug = require('gulp-pug');
-const del = require('del');
+const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
-// Таск для сборки Gulp файлов
-gulp.task('pug', function(callback) {
-	return gulp.src('./src/pug/pages/**/*.pug')
-		.pipe( plumber({
-			errorHandler: notify.onError(function(err){
-				return {
-					title: 'Pug',
-					sound: false,
-					message: err.message
-				}
-			})
-		}))
-		.pipe( pug({
-			pretty: true
-		}) )
-		.pipe( gulp.dest('./build/') )
-		.pipe( browserSync.stream() )
-	callback();
-});
-
-// Таск для компиляции SCSS в CSS
-gulp.task('scss', function(callback) {
-	return gulp.src('./src/scss/main.scss')
-		.pipe( plumber({
-			errorHandler: notify.onError(function(err){
-				return {
-					title: 'Styles',
-			        sound: false,
-			        message: err.message
-				}
-			})
-		}))
-		.pipe( sourcemaps.init() )
-		.pipe( sass() )
-		.pipe( autoprefixer({
-			overrideBrowserslist: ['last 4 versions']
-		}) )
-		.pipe( sourcemaps.write() )
-		.pipe( gulp.dest('./build/css/') )
-		.pipe( browserSync.stream() )
-	callback();
-});
-
-// Копирование Изображений
-gulp.task('copy:img', function(callback) {
-	return gulp.src('./src/img/**/*.*')
-	  .pipe(gulp.dest('./build/img/'))
-	callback();
-});
-
-// Копирование Скриптов
-gulp.task('copy:js', function(callback) {
-	return gulp.src('./src/js/**/*.*')
-	  .pipe(gulp.dest('./build/js/'))
-	callback();
-});
-
-// Слежение за HTML и CSS и обновление браузера
-gulp.task('watch', function() {
-
-	// Следим за картинками и скриптами и обновляем браузер
-	watch( ['./build/js/**/*.*', './build/img/**/*.*'], gulp.parallel(browserSync.reload) );
-
-	// Запуск слежения и компиляции SCSS с задержкой
-	watch('./src/scss/**/*.scss', function(){
-		setTimeout( gulp.parallel('scss'), 500 )
-	})
-
-	// Слежение за PUG и сборка
-	watch('./src/pug/**/*.pug', gulp.parallel('pug'))
-
-	// Следим за картинками и скриптами, и копируем их в build
-	watch('./src/img/**/*.*', gulp.parallel('copy:img'))
-	watch('./src/js/**/*.*', gulp.parallel('copy:js'))
-
-});
-
-// Задача для старта сервера из папки app
 gulp.task('server', function() {
-	browserSync.init({
-		server: {
-			baseDir: "./build/"
-		}
-	})
+
+    browserSync({
+        server: {
+            baseDir: "dist"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('clean:build', function() {
-	return del('./build')
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("dist/css"))
+        .pipe(browserSync.stream());
 });
 
-// Дефолтный таск (задача по умолчанию)
-// Запускаем одновременно задачи server и watch
-gulp.task(
-		'default', 
-		gulp.series( 
-			gulp.parallel('clean:build'),
-			gulp.parallel('scss', 'pug', 'copy:img', 'copy:js'), 
-			gulp.parallel('server', 'watch'), 
-			)
-	);
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
+    gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
+    gulp.watch("src/icons/**/*").on('all', gulp.parallel('icons'));
+    gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
+});
+
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('fonts', function () {
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('icons', function () {
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('images', function () {
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())
+        .pipe(gulp.dest("dist/img"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'images'));
